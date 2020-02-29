729efc9ab354aa16f021693dc96f4ff5a4bbce28
Karla Dayhoff <kddayhoff@gmail.com>
2020-02-29 17:30:59 -0600

    start button connected to timer which will countdown from 60 to 0 in one second increments

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-//Need to hide each section until it is called by clicking a button on the page before - Start Quiz for the first button will show questions 1, any of the buttons for the questions will hide that page and move on to the next question
-//Create 'function (rightAnswer)' [do I need to have a different named function for every question? Maybe not by using THIS] if else statements for answers to the questions (if button is clicked onClick() event), function validate() {
- /*var answer1 = document.getElementsByClassName("answer-button1");
- for (var i = 0, i < answer1.length; i++) {
-   if (answer[i].clicked) {
-     if( [i].value == "Cascading Style Sheets")
-        document.write("right-o-rini");
-     else
-        document.write("wrong-o");
-     break;
-   } } }   
-   ---will need a var array with ids #answer1a #answer1b etc
-   ---will need to add every right answer to in order to display the total score at the end 'var totalScore= []' <-- 
-   document.getElementById("score").innerHTML = totalScore.length; 
-//depending on the answer of the question before will determine whether  "Right-a-rini" or "wrong-o"  will show up under the question -- write to page - ex: if (question1 == answer2a);  write to page "correct", else write page "incorrect"
-//create a delay after showing the right or wrong then it will move on to the next sections
+var q1answers = ["1955","1985","1965","2155"];
+var q2answers = ["Jackie Brown","ReservoirDogs","Death Proof","Django"];
+var q3answers = ["Anchorman","Blues Brothers","Austin Powers","Tommy Boy"];
+var q4answers = ["Goldfinger","Live and Let Die","From Russia with Love","Diamonds are Forever"];
+var seconds = document.getElementById("seconds");
+var startBtn = document.createElement("button");
+startBtn.innerHTML = "Start Quiz!";
+
+startBtn.addEventListener("click", function() {
+ //change value below to 60 seconds to start countdown at 60  
+   var quizTimer = 5;
+   var startBtn = setInterval(function() {
+      quizTimer --;
+      seconds.textContent = quizTimer;
+      if(quizTimer === 0) {
+         clearInterval(startBtn);
+      }
+   }, 1000);
+   console.log(quizTimer);
+
+   function startBtn() {
+       document.createElement("h3") = --quizTimer;
+      }
+     });
+  document.body.appendChild(startBtn);
+
+  startBtn.addEventListener("click", function() {
+   
+  });
+
+
+/*document.getElementById("quiz");
+
+function answer1();
+for (var i = 0; i < q1answers.length; i++) {
+   if([0]==true);
+   document.write("right-o-rini");
+   else 
+      document.write("wrong-o");
+   //if this is true, add 10 sec to timer, if answer1 is false, remove 15 sec from timer 
+} */
