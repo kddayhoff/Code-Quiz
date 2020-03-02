2d6f8aecfb60e509cb8b25e5cb680e1198ba88b1
Karla Dayhoff <kddayhoff@gmail.com>
2020-03-02 16:12:33 -0600

    created funciton for getting content to page

--- pseudoscript.js
+++ pseudoscript.js
@@ -1,5 +1,3 @@
-//Create 'function (rightAnswer)' if else statements for answers to the questions (if button is clicked onClick() event), function validate() {
-  //variables for questions should be ARRAYS
  /*var answer1 = document.getElementsByClassName("answer-button1");
  for (var i = 0, i < answer1.length; i++) {
    if (answer[i].clicked) {
@@ -8,21 +6,13 @@
      else
         document.write("wrong-o");
      break;
-   } } }   
-   ---will need a var array with ids #answer1a #answer1b etc
-   ---will need to add every right answer to in order to display the total score at the end 'var totalScore= []' <-- 
-   document.getElementById("score").innerHTML = totalScore.length; 
+
+   document.getElementById("score").textcontent = totalScore.length; 
 //depending on the answer of the question before will determine whether  "Right-a-rini" or "wrong-o"  will show up under the question -- write to page (document.innterText or HTML) - ex: if (question1 == answer2a);  write to page "correct", else write page "incorrect"
-//create a delay after showing the right or wrong then it will move on to the next sections
 
 //DON'T HARD CODE IN HTML! PUT THINGS INTO VARIABLES! STRINGS OBJECTS! Make everything dynamic through HTML, looks aren't important - it's what's on the inside that counts
 //CREATE ELEMENT!!!
-//get the start button to attach to the timer
 
-//questions: -- 1) In what year did Marty travel backi in time to in the movie Back to the Future? a) 1955 b) 1985 c) 1965 d) 2155 
-//question 2: Which one of these is NOT a Quentin Tarantino Film: a) Jackie Brown b)Reservoir Dogs c)Death Proof d)Django
-//question 3: Which one of these movies is based on an SNL skit? a) Anchorman b) Blues Brothers c) Austin Powers  d) Tommy Boy
-//question 4: Which one of these James Bond films did Sean Connery NOT star in? a) Goldfinger b) Live and Let Die c) From Russia with Love d) Diamonds Are Forever
 //add push array to count how many right answers there (if true push to new arraw which will output on to page
 //will need to use local storage to save initials to page, jSon.parse will help avoid listing out each letter when putting on page, need to take list, it's an array (obj w/ scores) that are strings, getItemfrom local storage -- need to use for loop to turn it back into an array
 //'return' ex return a + b to show answer (return wrong/right). 
--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-var q1answers = ["1955","1985","1965","2155"];
-var q2answers = ["Jackie Brown","ReservoirDogs","Death Proof","Django"];
-var q3answers = ["Anchorman","Blues Brothers","Austin Powers","Tommy Boy"];
-var q4answers = ["Goldfinger","Live and Let Die","From Russia with Love","Diamonds are Forever"];
+
+var quiz = document.getElementById("quiz");
+var score = document.getElementById("score");
 var seconds = document.getElementById("seconds");
 var startBtn = document.createElement("button");
+var questions = [
+   {
+      text: "What year did Marty McFly go back in time to, in the movie <i>Back to the Future</i>?",
+      correctAnswer: "1955",
+      choices: ["1955", "1985","2055", "1965"]
+   },
+   {
+      text: "Which one of these is NOT a Quentin Tarantino Film?",
+      correctAnswer: "Django",
+      choices: ["Jackie Brown","Reservoir Dogs", "Death Proof", "Django"]
+   },
+   {
+      text: "Which one of these movies is based on a <i>Saturday Night Live</i> skit?",
+      correctAnswer: "Blues Brothers",
+      choices: ["Anchorman", "Blues Brothers", "Austin Powers", "Tommy Boy"]
+   },
+   {
+      text: "Which one of these James Bond films do Sean Connery NOT star in?",
+      correctAnswer: "Live and Let Die",
+      choices: ["Goldfinger", "Live and Let Die", "From Russia With Love", "Diamonds Are Forever"]
+   }
+]
+
+
+//for loop needs event to move on to next question
+
 startBtn.innerHTML = "Start Quiz!";
 
+
 startBtn.addEventListener("click", function() {
- //change value below to 60 seconds to start countdown at 60  
  event.preventDefault();
+ //change value below to 60 seconds to start countdown at 60  
    var quizTimer = 5;
    var startBtn = setInterval(function() {
       quizTimer --;
       seconds.textContent = quizTimer;
       if(quizTimer === 0) {
          clearInterval(startBtn);
+         document.write("Womp Womp, try again! Unless you're chicken...");
       }
    }, 1000);
    console.log(quizTimer);
@@ -24,24 +51,27 @@ startBtn.addEventListener("click", function() {
       }
      });
   document.body.appendChild(startBtn);
-  
-     //brings up first question of quiz simultaneously as the timer starts
-  startBtn.addEventListener("click", function() { 
-     var question1 = document.createElement("p");      question1.innerHTML = "What year did Marty McFly go back in time to, in the movie <i>Back to the Future</i>?";                
-      document.getElementById("question1").appendChild(question1);
+//end timer
+//start questions
+  var questionNumber = 0;
+  var finalScore = [];
+  function startQuiz(){
+     //quiz.innerHTML = questions[0].text;
+      //for (var i = 0; i < questions.text.length; i++); {
+        // console.log(i);
+         for (var i = 0; i < questions; i++) {
+            quiz.innerHTML = questions[i].text;
+            console.log(questions[i].choices);
+            console.log(questions[i].correctAnswer);
+      var questionChoices = createElement("button");
+      button.textContent = "questions.text";         //+= is x = x + y
+         var userChoices = document.createElement("button");
+         userChoices.innerHTML = questions[0].text[i];
+      }
+  }
 
-  });
-
-
-
-
-/*document.getElementById("quiz");
-
-function answer1();
-for (var i = 0; i < q1answers.length; i++) {
-   if([0]==true);
-   document.write("right-o-rini");
-   else 
-      document.write("wrong-o");
-   //if this is true, add 10 sec to timer, if answer1 is false, remove 15 sec from timer 
-} */
+  //document.body.appendChild(userChoices);
+   console.log(questionNumber);
+      console.log(questions);
+      console.log(questions[0]);
+   
