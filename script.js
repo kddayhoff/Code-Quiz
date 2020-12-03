40ab78c47683f1ad13b08aefa037b1dc0a994ace
Karla Dayhoff <kddayhoff@gmail.com>
2020-12-03 13:39:23 -0600

    made it so time disappears when quiz is finished and alert for time is up only shows if all questions are not answered by 0 timer

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var quizQuestions = document.getElementById("quizQuestions");
 var score = document.getElementById("score");
 var startBtn = document.createElement("button");
 var seconds = document.getElementById("seconds");
+var endTimer = document.getElementsByClassName("timer")
 var quizTimer = 60;
 var questionNumber = 0;
 var timerInterval;
 var finalScore = 0;
+var initials = document.getElementById("initials")
 var questions = [
    {
       ask: "What year did Marty McFly go back in time to, in the movie <i>Back to the Future</i>?",
@@ -35,24 +37,21 @@ startBtn.innerHTML = "Start Quiz!";
 startBtn.addEventListener("click", function () {
   event.preventDefault();
   // var quizTimer = 60;
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     quizTimer--;
     seconds.textContent = quizTimer;
-    if (quizTimer === 0) {
+    if (quizTimer <= 0) {
       clearInterval(timerInterval);
-      alert("Womp Womp, try again! Unless you're chicken...");
+      alert("Time's Up!");
+      endGame();
     }
   }, 1000);
   console.log(quizTimer);
 
-//   function startBtn() {
-//     //  document.getElementById("button") = --quizTimer;
-//     console.log("Show me the money");
-//   }
   showQuestions();
 });
   document.body.appendChild(startBtn);
- 
+ console.log(quizTimer)
 
 //Make questions appear on page and loop them until the user has gone through all choices
 function showQuestions () {
@@ -72,10 +71,11 @@ function showQuestions () {
           score.innerText = "Final score: " + finalScore;   
       });       
    } 
+};
    if (questionNumber === questions.length){
       endGame();
    }
-};
+
 }
 
 
@@ -95,106 +95,26 @@ function checkAnswers() {
       (userChoice !== questions[questionNumber].correctAnswer);
       questionNumber ++;
       quizTimer -= 15; 
+      console.log(quizTimer);
       console.log(questionNumber);
       document.getElementById("right-wrong").innerText = "wrong-o";
    }
 }
 
    function endGame() {
-      //create alerts for running out of time or finishing questions
-      //display none on what I don't want to show, 
+   clearInterval(timerInterval);
+   seconds.setAttribute("class", "d-none");
+   initials.removeAttribute("class", "d-none")
+   initials.setAttribute("class", "d-block")
+   //remove start quiz button here
+   //add submit button for initials
+
+     quizQuestions.innerHTML = "You Scored " + finalScore + " points!";
+     //ccall function for local storage
    }
 
+   //function here for local storage
 
 
-// var quizQuestions = document.getElementById("quizQuestions");
-// var score = document.getElementById("score");
-// var startBtn = document.createElement("button");
-// var seconds = document.getElementById("seconds");
 
-// var questionNumber = 0;
-// var finalScore = 0;
-// var questions = [
-//    {
-//       ask: "What year did Marty McFly go back in time to, in the movie <i>Back to the Future</i>?",
-//       correctAnswer: "1955",
-//       choices: ["1955", "1985","2055", "1965"]
-//    },
-//    {
-//       ask: "Which one of these is NOT a Quentin Tarantino Film?",
-//       correctAnswer: "Django",
-//       choices: ["Jackie Brown","Reservoir Dogs", "Death Proof", "Django"]
-//    },
-//    {
-//       ask: "Which one of these movies is based on a <i>Saturday Night Live</i> skit?",
-//       correctAnswer: "Blues Brothers",
-//       choices: ["Anchorman", "Blues Brothers", "Austin Powers", "Tommy Boy"]
-//    },
-//    {
-//       ask: "Which one of these James Bond films do Sean Connery NOT star in?",
-//       correctAnswer: "Live and Let Die",
-//       choices: ["Goldfinger", "Live and Let Die", "From Russia With Love", "Diamonds Are Forever"]
-//    }
-// ];
-
-// //Start Quiz timer
-// startBtn.innerHTML = "Start Quiz!";
-
-// startBtn.addEventListener("click", function() {
-//  event.preventDefault();
-//    var quizTimer = 60;
-//    var startBtn = setInterval(function() {
-//       quizTimer --;
-//       seconds.textContent = quizTimer;
-//       if(quizTimer === 0) {
-//          clearInterval(startBtn);
-//          alert("Womp Womp, try again! Unless you're chicken...");
-//       }
-//    }, 1000);
-//    console.log(quizTimer);
-
-//    function startBtn() {
-//        document.getElementById("button") = --quizTimer;
-//       }
-//      });
-//   document.body.appendChild(startBtn);
-
-// //Make questions appear on page and loop them until the user has gone through all choices
-// function showQuestions () {
-//    if (questionNumber < questions.length)  {
-//          quizQuestions.innerHTML = questions[questionNumber].ask;  
-//       };
-  
-//   for (var i = 0; i < questions[questionNumber].choices.length; i++) {
-//         console.log(questions[questionNumber]);
-       
-//        var btn = document.createElement("button");
-//        btn.innerText = questions[questionNumber].choices[i];
-//        quizQuestions.appendChild(btn);
-//        btn.addEventListener("click", function() {
-//           checkAnswers();
-//           showQuestions();
-//           score.innerText = "Final score: " + finalScore;   
-//       });       
-//    }
-// }
-// showQuestions()
-
-// function checkAnswers() {
-//    var userChoice = event.target.textContent;
-
-//    if (userChoice === questions[questionNumber].correctAnswer) {
-//       questionNumber ++;
-//       finalScore ++;
-//       document.getElementById("right-wrong").innerText = "right-o-rini!";
-//       console.log(questionNumber);
-//       console.log(finalScore);
-//    }
-//    else { 
-//       (userChoice !== questions[questionNumber].correctAnswer);
-//       questionNumber ++;
-//       console.log(questionNumber);
-//       document.getElementById("right-wrong").innerText = "wrong-o";
-//    }
-// }
 
