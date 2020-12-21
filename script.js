fc55c812badd32ba4a25a5b07e4d3d9e1d27ca5d
Karla Dayhoff <kddayhoff@gmail.com>
2020-12-21 11:07:46 -0600

    styling continued

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var quizQuestions = document.getElementById("quizQuestions");
 var score = document.getElementById("score");
 var startBtn = document.createElement("button");
 var seconds = document.getElementById("seconds");
-var endTimer = document.getElementsByClassName("timer")
+var endTimer = document.getElementsByClassName("timer");
 var initials = document.getElementById("initials");
-var startBtn = document.getElementById("start-button")
-var submitBtn = document.getElementById("submit-button")
+var startBtn = document.getElementById("start-button");
+var submitBtn = document.getElementById("submit-button");
 var rightWrong = document.getElementById("right-wrong");
-var savedInitials = document.getElementById("saved-initials")
+var savedInitials = document.getElementById("saved-initials");
 var savedScores = JSON.parse(localStorage.getItem("high-scores")) || [];
 var quizTimer = 60;
 var questionNumber = 0;
@@ -15,30 +15,36 @@ var timerInterval;
 var finalScore = 0;
 
 var questions = [
-   {
-      ask: "What year did Marty McFly go back in time to, in the movie <i>Back to the Future</i>?",
-      correctAnswer: "1955",
-      choices: ["1955", "1985","2055", "1965"]
-   },
-   {
-      ask: "Which one of these is NOT a Quentin Tarantino Film?",
-      correctAnswer: "Django",
-      choices: ["Jackie Brown","Reservoir Dogs", "Death Proof", "Django"]
-   },
-   {
-      ask: "Which one of these movies is based on a <i>Saturday Night Live</i> skit?",
-      correctAnswer: "Blues Brothers",
-      choices: ["Anchorman", "Blues Brothers", "Austin Powers", "Tommy Boy"]
-   },
-   {
-      ask: "Which one of these James Bond films do Sean Connery NOT star in?",
-      correctAnswer: "Live and Let Die",
-      choices: ["Goldfinger", "Live and Let Die", "From Russia With Love", "Diamonds Are Forever"]
-   }
+  {
+    ask:
+      "What year did Marty McFly go back in time to, in the movie <i>Back to the Future</i>?",
+    correctAnswer: "1955",
+    choices: ["1955", "1985", "2055", "1965"],
+  },
+  {
+    ask: "Which one of these is NOT a Quentin Tarantino Film?",
+    correctAnswer: "Django",
+    choices: ["Jackie Brown", "Reservoir Dogs", "Death Proof", "Django"],
+  },
+  {
+    ask:
+      "Which one of these movies is based on a <i>Saturday Night Live</i> skit?",
+    correctAnswer: "Blues Brothers",
+    choices: ["Anchorman", "Blues Brothers", "Austin Powers", "Tommy Boy"],
+  },
+  {
+    ask: "Which one of these James Bond films do Sean Connery NOT star in?",
+    correctAnswer: "Live and Let Die",
+    choices: [
+      "Goldfinger",
+      "Live and Let Die",
+      "From Russia With Love",
+      "Diamonds Are Forever",
+    ],
+  },
 ];
 
 //Start Quiz timer
-
 
 startBtn.addEventListener("click", function () {
   event.preventDefault();
@@ -56,90 +62,82 @@ startBtn.addEventListener("click", function () {
   startBtn.setAttribute("class", "d-none");
   showQuestions();
 });
- 
- console.log(quizTimer)
+
+console.log(quizTimer);
 
 //Make questions appear on page and loop them until the user has gone through all choices
-function showQuestions () {
-   if (questionNumber < questions.length)  {
-         quizQuestions.innerHTML = questions[questionNumber].ask;  
-     
-  
-  for (var i = 0; i < questions[questionNumber].choices.length; i++) {
-        console.log(questions[questionNumber]);
-       
-       var btn = document.createElement("button");
-       btn.innerText = questions[questionNumber].choices[i];
-       quizQuestions.appendChild(btn);
-       btn.addEventListener("click", function() {
-          checkAnswers();
-          showQuestions();
-          score.innerText = "Final score: " + finalScore;   
-      });       
-   } 
-};
-   if (questionNumber === questions.length){
-      endGame();
-   }
+function showQuestions() {
+  if (questionNumber < questions.length) {
+    quizQuestions.innerHTML = questions[questionNumber].ask;
 
+    for (var i = 0; i < questions[questionNumber].choices.length; i++) {
+      console.log(questions[questionNumber]);
+
+      var btn = document.createElement("button");
+      btn.innerText = questions[questionNumber].choices[i];
+      quizQuestions.appendChild(btn);
+      btn.addEventListener("click", function () {
+        checkAnswers();
+        showQuestions();
+        score.innerText = "Final score: " + finalScore;
+      });
+    }
+  }
+  if (questionNumber === questions.length) {
+    endGame();
+  }
 }
-
 
 function checkAnswers() {
-   var userChoice = event.target.textContent;
+  var userChoice = event.target.textContent;
 
-   if (userChoice === questions[questionNumber].correctAnswer) {
-      questionNumber ++;
-      finalScore ++;
-      quizTimer += 10;
-      console.log(quizTimer);
-      rightWrong.innerText = "Right-O-Rini!";
-      console.log(questionNumber);
-      console.log(finalScore);
-   }
-   else { 
-      (userChoice !== questions[questionNumber].correctAnswer);
-      questionNumber ++;
-      quizTimer -= 15; 
-      console.log(quizTimer);
-      console.log(questionNumber);
-      rightWrong.innerText = "Wrong-O";
-   }
+  if (userChoice === questions[questionNumber].correctAnswer) {
+    questionNumber++;
+    finalScore++;
+    quizTimer += 10;
+    console.log(quizTimer);
+    rightWrong.innerText = "Right-O-Rini!";
+    console.log(questionNumber);
+    console.log(finalScore);
+  } 
+  
+  else {
+    userChoice !== questions[questionNumber].correctAnswer;
+    questionNumber++;
+    quizTimer -= 15; 
+    rightWrong.innerText = "Wrong-O";
+    console.log(quizTimer);
+    console.log(questionNumber);
+   
+  }
 }
 
-   function endGame() {
-   clearInterval(timerInterval);
-   seconds.setAttribute("class", "d-none");
-   initials.removeAttribute("class", "d-none");
-   initials.setAttribute("class", "d-block");
-   submitBtn.setAttribute("class", "d-block")
-   rightWrong.setAttribute("class", "d-none")
-   //add submit button for initials
-   
-     quizQuestions.innerHTML = "You Scored " + finalScore + " points!";
-     //ccall function for local storage
-     submitBtn.addEventListener("click", function () {
-        var userInitials = initials.value;
-        if (userInitials == null || userInitials == ""){
-         alert("Please Enter Your Initials");
-         return false;
-        }
-        savedScores.push("Name: " + userInitials + " | " + " Score: " + finalScore);
-        localStorage.setItem("high-scores", JSON.stringify(savedScores));
-displayHighScores()
-     })
-     
-   }
+function endGame() {
+  clearInterval(timerInterval);
+  seconds.setAttribute("class", "d-none");
+  initials.removeAttribute("class", "d-none");
+  initials.setAttribute("class", "d-block");
+  submitBtn.setAttribute("class", "d-block");
+  rightWrong.setAttribute("class", "d-none");
+ 
+  quizQuestions.innerHTML = "You Scored " + finalScore + " points!";
 
-   function displayHighScores () {
-      for (var i = 0; i < savedScores.length; i++){
-         var scoreList = document.createElement("li")
-         scoreList.innerText = savedScores[i];
-         savedInitials.appendChild(scoreList)
-      }
+  submitBtn.addEventListener("click", function () {
+    var userInitials = initials.value;
+    if (userInitials == null || userInitials == "") {
+      alert("Please Enter Your Initials");
+      return false;
+    }
+    savedScores.push("Name: " + userInitials + " | " + " Score: " + finalScore);
+    localStorage.setItem("high-scores", JSON.stringify(savedScores));
+    displayHighScores();
+  });
+}
 
-   }
-
-
-
-
+function displayHighScores() {
+  for (var i = 0; i < savedScores.length; i++) {
+    var scoreList = document.createElement("li");
+    scoreList.innerText = savedScores[i];
+    savedInitials.appendChild(scoreList);
+  }
+}
